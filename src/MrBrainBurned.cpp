bd4e6b7a7503db9dd5b6c03cd7a94d9f430786b0
Wenrurumon <askahu6@gmail.com>
2016-12-01 17:04:21 -0600

    Update MrBrainBurned.cpp

--- src/MrBrainBurned.cpp
+++ src/MrBrainBurned.cpp
@@ -3,46 +3,71 @@
 #include <Rcpp.h>
 #include <math.h>
 #include <algorithm>
+#include <vector>
 
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 using namespace std;
 using namespace arma;
 
-//main
+//[[Rcpp::Export]]
 int main(){
   cout << "Main Function" << endl;
   return 0;
 }
 
 //pool
+
 class pooling{
 private:
-  vector<int> pool;
+  vector<int> pool, player1, player2;
 public:
-  void init(vector<int> x){
-    pool = x;
+  void init(){
+    for (int i = 0; i < 13; i++){
+      pool.insert(pool.end(),4,i+1);
+    }
+    vector<int> player1, player2;
   }
-  void kick(int x){
-    pool.erase(std::remove(pool.begin(), pool.end(), x), pool.end());
-  }
-  vector<int> output(){
+  vector<int> outpool(){
     return pool;
+  }
+  vector<int> outp1(){
+    return player1;
+  }
+  vector<int> outp2(){
+    return player2;
+  }
+  void carding(int x){
+    int psize = pool.size();
+    int pick = rand() % psize;
+    if(x == 1){
+      player1.insert(player1.end(),pick);
+    } else {
+      player2.insert(player2.end(),pick);
+    }
+    pool.erase(pool.begin+pick);
   }
 };
 
 //[[Rcpp::export]]
-vector<int> p1(vector<int> x){
-  pooling p;
-  p.init(x);
-  p.kick(10);
-  return p.output();
+SEXP test(){
+  pooling x;
+  x.init();
+  return List::create(_["P"]=x.outpool(),
+                      _["p1"]=x.outp1(),
+                      _["p2"]=x.outp2())
 }
-  
-  
-//export
-RCPP_MODULE(m){
-  using namespace Rcpp;
-  function("test", &p1);
-  function("main", &main);
+
+//[[Rcpp::export]]
+SEXP test2(){
+  pooling x;
+  x.init();
+  x.carding(1);
+  return List::create(_["P"]=x.outpool(),
+                      _["p1"]=x.outp1(),
+                      _["p2"]=x.outp2())
 }
+
+
+
+
