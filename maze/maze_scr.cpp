8e59d92035a71ff520a2819413563ebbac451080
進撃の雯雯老師 <askahu6@gmail.com>
2017-02-27 23:02:56 -0600

    Update maze_scr.cpp

--- maze/maze_scr.cpp
+++ maze/maze_scr.cpp
@@ -7,6 +7,40 @@ using namespace arma;
 using namespace Rcpp;
 using namespace std;
 
+//Macros
+//[[Rcpp::export]]
+vector<int> get_position(int i, mat & x){
+  vector<int> rlt(2);
+  int ncol = x.n_cols, nrow=x.n_rows;
+  if (ncol*nrow<i){
+    cout << "out of range" << endl;
+  }
+  int icol = (i-1)/nrow;
+  int irow = i - icol*ncol;
+  rlt[0] = irow+1; rlt[1] = icol;
+  return rlt;
+}
+
+//[[Rcpp::export]]
+int get_point(vector<int> p, mat & x){
+  int nrow=x.n_rows;
+  int rlt = (p[1]-1)*nrow+p[0];
+  return rlt;
+}
+
+//[[Rcpp::export]]
+mat randmaze(int nrow, int ncol, double pna, int penval){
+  mat rlt = zeros(nrow,ncol);
+  for(int i=1; i<nrow*ncol-1; i++){
+    double rval=(rand()%100), thres= 100 - pna*100;
+    if(rval>thres){
+      rlt[i] = penval;
+    }
+  }
+  rlt[nrow*ncol-1] = 1;
+  return rlt;
+}
+
 //Class
 class maze{
 private:
@@ -24,29 +58,18 @@ public:
   }
 };
 
-
-//Macros
-
-vector<int> get_position(int i, mat & x){
-  vector<int> rlt(2);
-  int ncol = x.n_cols, nrow=x.n_rows;
-  if (ncol*nrow<i){
-    cout << "out of range" << endl;
-  }
-  int irow = (i-1)/nrow;
-  int icol = i - irow*ncol;
-  rlt[0] = irow+1; rlt[1] = icol;
-  return rlt;
+//Main
+int test(mat m){
+  maze m1, m2;
+  m1.init();
+  m2.init(m);
+  cout << m1.display() << endl;
+  cout << m2.display() << endl;
+  return 0;
 }
 
-mat randmaze(int nrow, int ncol, double pna, int penval){
-  mat rlt = zeros(nrow,ncol);
-  for(int i=1; i<nrow*ncol-1; i++){
-    double rval=(rand()%100), thres= 100 - pna*100;
-    if(rval>thres){
-      rlt[i] = penval;
-    }
-  }
-  rlt[nrow*ncol-1] = 1;
-  return rlt;
+//Export
+RCPP_MODULE(unif_module){
+  // function("test",&test);
 }
+
